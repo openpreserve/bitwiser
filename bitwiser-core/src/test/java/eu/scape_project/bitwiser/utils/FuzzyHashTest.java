d3babae91954d2cf39595691686fb80548133699
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-02-04 22:48:25 +0000

    Added a test case.

---
+++ bitwiser-core/src/test/java/eu/scape_project/bitwiser/utils/FuzzyHashTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package eu.scape_project.bitwiser.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * @author Andrew Jackson <Andrew.Jackson@bl.uk>
+ *
+ */
+public class FuzzyHashTest {
+
+    @Test
+    public void testComparison() {
+        FuzzyHash fh1 = FuzzyHash.fromString(
+                "96:Axd6wa9IrXjodTkt8yJksfGwr9+grnw8LoCQBkhrmsl7OnsCYBaAcIjz:c6wvodwt5AkHLoDOhrUnsbBaEjz,\"README.md\"");
+        FuzzyHash fh2 = FuzzyHash.fromString(
+                "96:qxd6wa9IrXjodTkt8yJksfGwr9+grnw8LoCQBkhrmsl7OnsCYBaI:q6wvodwt5AkHLoDOhrUnsbBaI,\"README-2.md\"");
+        System.out.println("FH1: " + fh1.toString());
+        System.out.println("FH2: " + fh2.toString());
+        int comparison = FuzzyHash.compare(fh1, fh2);
+        System.out.println("Comparison = " + comparison);
+        assertEquals("Wrong comparisong result!", 97, comparison);
+    }
+
+    @Test
+    public void testComparisonWithoutNames() {
+        FuzzyHash fh1 = FuzzyHash.fromString(
+                "96:Axd6wa9IrXjodTkt8yJksfGwr9+grnw8LoCQBkhrmsl7OnsCYBaAcIjz:c6wvodwt5AkHLoDOhrUnsbBaEjz");
+        FuzzyHash fh2 = FuzzyHash.fromString(
+                "96:qxd6wa9IrXjodTkt8yJksfGwr9+grnw8LoCQBkhrmsl7OnsCYBaI:q6wvodwt5AkHLoDOhrUnsbBaI");
+        System.out.println("FH1: " + fh1.toString());
+        System.out.println("FH2: " + fh2.toString());
+        int comparison = FuzzyHash.compare(fh1, fh2);
+        System.out.println("Comparison = " + comparison);
+        assertEquals("Wrong comparisong result!", 97, comparison);
+    }
+}
